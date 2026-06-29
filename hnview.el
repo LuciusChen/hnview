1876200ad573c41e8b14bcb66a236182d13ced35
Lucius <chenyh572@gmail.com>
2026-06-29 09:47:22 +0800

    Add GPL license metadata for MELPA

--- hnview.el
+++ hnview.el
@@ -1,11 +1,26 @@
 ;;; hnview.el --- Modern translated Hacker News reader -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2026 Lucius Chen
+
 ;; Author: Lucius Chen
+;; Assisted-by: OpenAI Codex:gpt-5
 ;; URL: https://github.com/luciuschen/hnview
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "29.1") (llm "0.30.1") (plz "0.9"))
 ;; Keywords: news, hypermedia, convenience
-;; Assisted-by: OpenAI Codex (GPT-5)
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
